3400db06bd1401c08a24fd20679e8945dde2eeb9
Jelani Woods <jelani@firstdraft.com>
2021-06-26 20:53:01 -0500

    Init
    Challenge 2.4

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    private float spawnPosZ = 20;
+    private float spawnRangeX = 20;
+
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnRandomAnimal()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        GameObject randomAnimal = animalPrefabs[animalIndex];
+        Instantiate(randomAnimal, spawnPos, randomAnimal.transform.rotation);
+    }
+
+}
